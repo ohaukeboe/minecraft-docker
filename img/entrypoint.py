4da7257fe8faf2b965316f1faa45215071742e04
Magnus <mhaukeboe@hotmail.com>
2022-12-29 11:56:57 +0100

    fixed

--- img/entrypoint.py
+++ img/entrypoint.py
@@ -4,6 +4,12 @@ import os
 import wget
 import tarfile
 
+verbose = os.environ.get('VERBOSE')
+
+def verbosePrint (string):
+    if verbose:
+        print(string)
+
 bkup = os.environ.get('WORLD_BACKUP')
 mods = os.environ.get('MODS_BACKUP')
 plug = os.environ.get('PLUGINS_BACKUP')
@@ -11,53 +17,56 @@ plug = os.environ.get('PLUGINS_BACKUP')
 lines = open('/minecraft/properties', 'r').readlines()
 serverProperties = open('/minecraft/server.properties', 'w')
 
+verbosePrint('Generating server.properties')
 for line in lines:
     line = line.strip('\n')
     if not line[0]=='#':
         line = line.split('=')
+        verbosePrint('Looking for: ' + line[0].upper().replace('-', '_'))
         environ = os.environ.get(line[0].upper().replace('-', '_'))
         if not environ:
             environ = line[1]
-
+        verbosePrint('writing ' + environ)
         newLine = line[0]+'='+environ
         serverProperties.write(newLine+'\n')
     else:
         serverProperties.write(line+'\n')
 
 serverProperties.close()
+verbosePrint('server.properties generated')
 
 # Add default world
 if bkup:
     if not os.path.exists('/minecraft/world'):
-        print ('Installing default world', bkup)
+        verbosePrint ('Installing default world', bkup)
         os.mkdir('/minecraft/world')
 
         wget.download(bkup, out='/tmp/world.tar.gz')
         tarfile.extractall('/tmp/world.tar.gz')
         os.remove('/tmp/world.tar.gz')
-        print ('Done installing default world', bkup)
+        verbosePrint ('Done installing default world', bkup)
 
 # Add plugins
 # if plug:
 #     if not os.path.exists('/minecraft/plug'):
-#         print ('Installing default plug', plug)
+#         verbosePrint ('Installing default plug', plug)
 #         os.mkdir('/minecraft/plug')
 
 #         wget.download(plug, out='/tmp/plug.tar.gz')
 #         tarfile.extractall('/tmp/plug.tar.gz')
 #         os.remove('/tmp/plug.tar.gz')
-#         print('Done installing plug')
+#         verbosePrint('Done installing plug')
 
 # add mods
 if mods:
     if not os.path.exists('/minecraft/mods'):
-        print ('Installing default mods', mods)
+        verbosePrint ('Installing default mods', mods)
         os.mkdir('/minecraft/mods')
 
         wget.download(mods, out='/tmp/mods.tar.gz')
         tarfile.extractall('/tmp/mods.tar.gz')
         os.remove('/tmp/mods.tar.gz')
-        print('Done installing mods')
+        verbosePrint('Done installing mods')
 
 # add config
 if not os.path.exists('/minecraft/config'):
