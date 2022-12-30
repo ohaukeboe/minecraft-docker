399cd9f23b0c38e977bee69a50cd81de1811a54c
Oskar Haukeboe <ohaukeboe@pm.me>
2022-12-30 11:11:24 +0100

    Small improvement on restore.py

--- img/restore.py
+++ img/restore.py
@@ -9,18 +9,28 @@ import websockets as ws
 backup_type = sys.argv[1]
 backup_date = sys.argv[2]
 
-filename = '/minecraft/config/backup/' + backup_type + '/' + backup_date + '.tar.gz'
-extract_dir = '/minecraft/' + backup_type
+filename = "/minecraft/config/backup/" + backup_type + "/" + backup_date + ".tar.gz"
+extract_dir = "/minecraft/" + backup_type
+
+if not os.path.exists("/minecraft/config/backup/" + backup_type):
+    print("No backups available for type '" + backup_type + "'")
+    exit()
+if not os.path.exists(filename):
+    print("No backup found for", backup_date)
+    exit()
+
 
 async def send():
-    uri = 'ws://127.0.0.1:18080'
+    uri = "ws://127.0.0.1:18080"
     async with ws.connect(uri) as websocket:
-        await websocket.send('stop')
+        await websocket.send("stop")
         await websocket.recv()
-        sh.rmtree('/minecraft/' + backup_type, ignore_errors=True)
-        sh.unpack_archive(filename, '/minecraft')
-        await websocket.send('start')
+        print("Restoring...")
+        sh.rmtree("/minecraft/" + backup_type, ignore_errors=True)
+        sh.unpack_archive(filename, "/minecraft")
+        await websocket.send("start")
         await websocket.recv()
+        print("Done restoring")
+
 
 asy.run(send())
-
